423190bbe28ea18d905c4c47449c046d63a68e2d
Dan Munro <dan@danmunro.com>
2012-08-03 07:27:56 -0700

    Removing unnecessary bound variable

--- examples/chatroom.php
+++ examples/chatroom.php
@@ -8,7 +8,7 @@ $clients = [];
 $beehive->setConnectCallback(function($client) use (&$clients) {
 	$clients[] = $client;
 });
-$beehive->setReadCallback(function($sender, $input) use ($beehive, &$clients) {
+$beehive->setReadCallback(function($sender, $input) use (&$clients) {
 	// tell the server to echo any input back as output
 	foreach($clients as $client) {
 		$client->write('[client '.$sender->getID()."] ".$input."\r\n");
